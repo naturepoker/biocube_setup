47664e661650388b160e61fb3b35e11bfcf81659
naturepoker <sung@binomicalabs.org>
2024-07-02 02:18:22 -0400

    adding dnf&zypper, reorganizing to sys vs compile targets

--- compilation_list/compilation_list.sh
+++ compilation_list/compilation_list.sh
@@ -2,46 +2,19 @@
 
 setup_date=$(date +"%Y%m%d")
 
-
-if [[ $(uname -p) == x86_64 ]]; then
-	printf "\n Compatible CPU architecture found"
-else
-	printf "\n Incompatible architecture found. This setup script is meant for x86_64 machines"
-	exit
-fi
-
-if ! command -v apt > /dev/null; then
-	printf "\n This script is meant for Debian/Ubuntu systems with apt command. Exiting"
-	exit
-fi
-
-
 echo "##################################################"
 echo "##################################################"
 echo "##################################################"
 echo "                                                  "
-echo "           SCRIPT UPDATED - NOV 12 2023           "
+echo "           SCRIPT UPDATED - JUN 22 2024           "
 echo "                                                  "
 echo "##################################################"
 
 cd
-sudo apt update
-sudo apt upgrade
-sudo apt install autoconf autoconf-doc automake bison build-essential cmake colordiff curl default-jdk dictd dict-freedict-fra-eng dict-gcide ecasound enscript ffmpeg flac fontconfig gcc gdb gfortran git gnuplot grads graphviz groff gtk-doc-tools htop imagemagick jq lame lftp libboost-all-dev libbz2-dev libcurl4-gnutls-dev libeigen3-dev libexif-dev libffi-dev libfribidi-dev libfuse2 libgc-dev libgdk-pixbuf2.0-dev libgif-dev libglib2.0-dev libgmp-dev libgsl-dev libgtk-3-dev libimage-exiftool-perl libimlib2-dev liblapack-dev liblzma-dev libmagickwand-dev libmpc-dev libmpfr-dev libmpg123-dev libncurses5-dev libopenblas-dev libpcre3-dev libreadline-dev libsdl2-dev libssl-dev libtool libunistring-dev libvirt-daemon libxft-dev lm-sensors lynx m4 m4-doc make mencoder mpv neofetch ninja-build openssh-client openssh-server p7zip-full p7zip-rar parallel perl pv python3-pip python3-testresources python-is-python3 qemu-kvm rlwrap sox sudo texlive-fonts-recommended texlive-plain-generic texlive-xetex unrar unzip virt-manager vlc vorbis-tools xclip zathura zathura-djvu zathura-ps zlib1g zlib1g-dev
 
 mkdir ~/tools
 cd ~/tools
-mkdir bin
 touch "$setup_date"_setup.log
-
-echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-echo "###Setting up bedtools2###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-git clone https://github.com/arq5x/bedtools2
-cd bedtools2
-make -j 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-cp bin/* ~/tools/bin
-cd ~/tools
 
 echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 echo "###Setting up htslib###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
@@ -55,6 +28,17 @@ make -j 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 sudo make install
 cd ~/tools
 rm -rf htslib
+
+sudo ldconfig
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up bedtools2###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/arq5x/bedtools2
+cd bedtools2
+make -j 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+cp bin/* ~/.local/bin
+cd ~/tools
 
 echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 echo "###Setting up samtools###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
@@ -88,7 +72,7 @@ echo "###Setting up pfam-A###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 mkdir pfam
 cd pfam
-wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz
+lftp -c get ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz
 gunzip Pfam-A.hmm.gz
 hmmpress Pfam-A.hmm 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 cd ~/tools
@@ -99,7 +83,7 @@ echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 git clone https://github.com/lh3/seqtk
 cd seqtk
 make -j 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-mv seqtk ~/tools/bin
+mv seqtk ~/.local/bin
 cd ~/tools
 rm -rf seqtk
 
@@ -108,8 +92,10 @@ echo "###Setting up filtlong###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 git clone https://github.com/rrwick/Filtlong.git
 cd Filtlong
+#echo "possible gcc13 build failure - add -#include <cstdint>- to kmer.h"
+#https://github.com/rrwick/Filtlong/pull/39
 make -j 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-mv bin/filtlong ~/tools/bin
+mv bin/filtlong ~/.local/bin
 cd ~/tools
 rm -rf Filtlong
 
@@ -119,7 +105,7 @@ echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 git clone https://github.com/lh3/minimap2
 cd minimap2
 make -j 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-mv minimap2 ~/tools/bin
+mv minimap2 ~/.local/bin
 cd ~/tools
 rm -rf minimap2
 
@@ -129,7 +115,7 @@ echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 git clone https://github.com/lh3/miniprot
 cd miniprot
 make -j 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-mv miniprot ~/tools/bin
+mv miniprot ~/.local/bin
 cd ~/tools
 rm -rf miniprot
 
@@ -139,7 +125,7 @@ echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 git clone https://github.com/lh3/bwa
 cd bwa
 make -j 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-mv bwa ~/tools/bin
+mv bwa ~/.local/bin
 cd ~/tools
 rm -rf bwa
 
@@ -149,7 +135,7 @@ echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 git clone https://gitlab.com/mcfrith/last
 cd last
 make -j 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-cp bin/* ~/tools/bin
+cp bin/* ~/.local/bin
 cd ~/tools
 rm -rf last
 
@@ -159,7 +145,7 @@ echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 git clone https://github.com/lh3/bioawk
 cd bioawk
 make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-cp bioawk ~/tools/bin
+cp bioawk ~/.local/bin
 cd ~/tools
 rm -rf bioawk
 
@@ -169,9 +155,9 @@ echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 git clone https://github.com/inab/trimal
 cd trimal/source
 make -j 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-mv trimal ~/tools/bin
-mv statal ~/tools/bin
-mv readal ~/tools/bin
+mv trimal ~/.local/bin
+mv statal ~/.local/bin
+mv readal ~/.local/bin
 cd ~/tools
 rm -rf trimal
 
@@ -179,7 +165,6 @@ echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 echo "###Setting up iqtree2###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 git clone https://github.com/iqtree/iqtree2
-mkdir iqtree2/build
 cd iqtree2/
 #iqtree2/1 git repo seems to require manual submodule init and update - otherwise compilation will fail due to missing lsd2
 #'git checkout latest' might be needed here too (Sep 19 2022)
@@ -188,8 +173,8 @@ git submodule update
 mkdir build
 cd build
 cmake ..
-make -j 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-mv iqtree2 ~/tools/bin
+make -j 1 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+mv iqtree2 ~/.local/bin
 cd ~/tools
 rm -rf iqtree2
 
@@ -198,8 +183,8 @@ echo "###Setting up muscle5###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 git clone https://github.com/rcedgar/muscle
 cd muscle/src
-make -j 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-cp Linux/muscle ~/tools/bin
+make -j 1 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+cp Linux/muscle ~/.local/bin
 cd ~/tools
 rm -rf muscle
 
@@ -211,72 +196,73 @@ cd FastANI
 ./bootstrap.sh 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 ./configure 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
 make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
-cp fastANI ~/tools/bin
+cp fastANI ~/.local/bin
 cd ~/tools
 rm -rf FastANI
 
 wget http://www.microbesonline.org/fasttree/FastTreeMP
 chmod +x FastTreeMP
-mv FastTreeMP ~/tools/bin
+mv FastTreeMP ~/.local/bin
 
 wget http://opengene.org/fastp/fastp
 chmod +x fastp
-mv fastp ~/tools/bin
+mv fastp ~/.local/bin
 
-wget https://github.com/shenwei356/taxonkit/releases/download/v0.14.1/taxonkit_linux_amd64.tar.gz
+wget https://github.com/shenwei356/taxonkit/releases/download/v0.16.0/taxonkit_linux_amd64.tar.gz
 tar zxvf taxonkit_linux_amd64.tar.gz
 rm taxonkit_linux_amd64.tar.gz
-mv taxonkit ~/tools/bin
+mv taxonkit ~/.local/bin
 
-wget -c ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
+lftp -c get ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
 tar zxvf taxdump.tar.gz
 mkdir -p $HOME/.taxonkit
 mv names.dmp nodes.dmp delnodes.dmp merged.dmp $HOME/.taxonkit
 rm citations.dmp division.dmp gc.prt gencode.dmp images.dmp readme.txt taxdump.tar.gz
 
-wget https://github.com/shenwei356/seqkit/releases/download/v2.4.0/seqkit_linux_amd64.tar.gz
+wget https://github.com/shenwei356/seqkit/releases/download/v2.8.2/seqkit_linux_amd64.tar.gz
 tar zxvf seqkit_linux_amd64.tar.gz
 rm seqkit_linux_amd64.tar.gz
-mv seqkit ~/tools/bin
+mv seqkit ~/.local/bin
 
 wget https://mmseqs.com/latest/mmseqs-linux-sse41.tar.gz
 tar zxvf mmseqs-linux-sse41.tar.gz
-ln -s ~/tools/mmseqs/bin/mmseqs ~/tools/bin/mmseqs
+ln -s ~/tools/mmseqs/bin/mmseqs ~/.local/bin/mmseqs
 rm mmseqs-linux-sse41.tar.gz
 
 git clone https://github.com/mpdunne/alan
-mv alan/alan ~/tools/bin
+mv alan/alan ~/.local/bin
 rm -rf alan
 
 wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.15.0+-x64-linux.tar.gz
 tar zxvf ncbi-blast-2.15.0+-x64-linux.tar.gz
-cp ncbi-blast-2.15.0+/bin/* ~/tools/bin
+cp ncbi-blast-2.15.0+/bin/* ~/.local/bin
 rm -rf ncbi-blast-2.15.0+-x64-linux.tar.gz ncbi-blast-2.15.0+
 
-sh -c "$(wget -q ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/install-edirect.sh -O -)"
+#wget through ftp does not seem to work anymore on fedora 40 onward
+#sh -c "$(wget -q ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/install-edirect.sh -O -)"
+lftp -c get ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/install-edirect.sh
+chmod +x install-edirect.sh
+./install-edirect.sh
+rm install-edirect.sh
 
-wget https://github.com/makeworld-the-better-one/amfora/releases/download/v1.9.2/amfora_1.9.2_linux_64-bit
-mv amfora_1.9.2_linux_64-bit amfora
+wget https://github.com/makew0rld/amfora/releases/download/v1.10.0/amfora_1.10.0_linux_64-bit
+mv amfora_1.10.0_linux_64-bit amfora
 chmod +x amfora
-mv amfora ~/tools/bin
+mv amfora ~/.local/bin
 
 git clone https://github.com/muennich/sxiv
 cd sxiv
 make -j
-mv sxiv ~/tools/bin
+mv sxiv ~/.local/bin
 cd ~/tools
 rm -rf sxiv
-
-wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
-chmod +x yt-dlp
-mv yt-dlp ~/tools/bin
 
 git clone --recursive --branch release https://git.skyjake.fi/gemini/lagrange
 mkdir lagrange_browser
 cd lagrange_browser
 cmake ~/tools/lagrange -DCMAKE_BUILD_TYPE=Release
 cmake --build .
-ln -s ~/tools/lagrange_browser/lagrange ~/tools/bin/lagrange
+ln -s ~/tools/lagrange_browser/lagrange ~/.local/bin/lagrange
 cd ~/tools
 rm -rf lagrange
 
@@ -287,30 +273,42 @@ source ~/.bashrc
 git clone https://github.com/atanunq/viu
 cd viu
 cargo install --path .
-cp target/release/viu ~/tools/bin
+cp target/release/viu ~/.local/bin
 cd ~/tools
 rm -rf viu
 
-git clone https://github.com/rrwick/Polypolish.git
-cd Polypolish
+git clone https://github.com/mbhall88/rasusa.git
+cd rasusa
 cargo build --release
-mv target/release/polypolish ~/tools/bin
+cargo test --all
+cp target/release/rasusa ~/.local/bin/
 cd ~/tools
-rm -rf Polypolish
+rm -rf rasusa
 
 git clone https://github.com/fulcrumgenomics/fqgrep
 cd fqgrep
 cargo build --release
-mv target/release/fqgrep ~/tools/bin
+mv target/release/fqgrep ~/.local/bin
 cd ~/tools
 rm -rf fqgrep
 
-pip3 install pyfaidx
-pip3 install biopython
-pip3 install ncbi-genome-download
-pip3 install bpytop
-pip3 install pipenv
-pip3 install html2text
+pipx install pyfaidx
+#ncbi-genome-download deprecated for https://github.com/AstrobioMike/bit
+pipx install bpytop
+pipx install html2text
+pipx install termvisage
+
+#alternate shell choices - wezterm vs kitty
+#curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+#ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/kitty
+
+git clone https://github.com/hackerb9/lsix
+mv lsix/lsix ~/.local/bin
+rm -rf lsix
+
+git clone https://github.com/hackerb9/vv
+cp vv/vv ~/.local/bin
+rm -rf vv
 
 curl -fsSL https://install.julialang.org | sh
 
@@ -319,8 +317,6 @@ echo "##################################################"
 echo "##################################################"
 echo "## You might want to add below paths to .bashrc ##"
 echo "                                                  "
-echo 'export PATH=${PATH}:$HOME/tools/bin               '
-echo 'export PATH=${PATH}:$HOME/.local/bin              '
 echo 'export PATH=${PATH}:$HOME/tools/scripts/sung-tools'
 echo "                                                  "
 echo "###### Also consider installing pandoc via  ######"
@@ -328,6 +324,32 @@ echo "##### https://github.com/jgm/pandoc/releases #####"
 echo "                                                  "
 echo "######## And consider bioconda setup from ########"
 echo "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+echo "or mamba alternateive!                            "
 echo "###### conda config --add channels defaults ######"
 echo "###### conda config --add channels bioconda ######"
 echo "#### conda config --add channels conda-forge #####"
+echo "##################################################"
+echo "##################################################"
+
+cat << EOF
+#julia environment setup
+#.julia/config/startup.jl
+add OhMyREPL, Revise
+
+atreplinit() do repl
+try
+	@eval using OhMyREPL
+	catch e
+	@warn "error while importing OhMyREPL" exception=(e, catch_backtrace())
+end
+end
+
+atreplinit() do repl
+try
+	@eval using Revise
+	catch e
+	@warn "error while importing Revise" exception=(e, catch_backtrace())
+end
+end
+
+EOF
---
+++ dnf-os/fedora_base_setup.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+setup_date=$(date +"%Y%m%d")
+
+if ! command -v dnf > /dev/null; then
+	printf "\n This script is meant for a distro with DNF command. Exiting"
+	exit
+fi
+
+
+echo "##################################################"
+echo "##################################################"
+echo "##################################################"
+echo "                                                  "
+echo "           SCRIPT UPDATED - JUN 22 2024           "
+echo "                                                  "
+echo "##################################################"
+
+cd
+
+#assumes manual dnf update and initial upgrade & refresh
+#echo "max_parallel_downloads=20" >>  /etc/dnf/dnf.conf
+#sudo dnf -y upgrade --refresh
+#reboot
+
+#adding free and nonfree fusion repos
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf makecache
+sudo dnf upgrade --refresh
+sudo dnf -y groupupdate core
+sudo dnf install -y rpmfusion-free-release-tainted
+sudo dnf install -y rpmfusion-nonfree-release-tainted
+sudo dnf install -y dnf-plugins-core
+sudo dnf config-manager --set-enabled fedora-cisco-openh264
+sudo dnf install -y gstreamer1-plugin-openh264 mozilla-openh264
+sudo dnf install lame\* --exclude=lame-devel
+sudo dnf install -y fira-code-fonts 'mozilla-fira*' 'google-roboto*'
+#if using nvidia drivers:
+#sudo dnf install akmod-nvidia
+#sudo dnf install xorg-x11-drv-nvidia-cuda
+
+#removing pycharm copr (requires dnf-plugins-core) - this one's only present as default on workstation version
+sudo dnf copr remove phracek/PyCharm
+
+sudo dnf groupinstall -y "C Development Tools and Libraries"
+sudo dnf groupinstall -y "Development Tools" "Development Libraries"
+sudo dnf groupinstall -y "Multimedia"
+
+sudo dnf install -y imlib2-devel p7zip p7zip-plugins cmake colordiff datamash eigen3-devel flac flatpak gdk-pixbuf2-devel giflib-devel parallel gnuplot gsl-devel pipx htop ImageMagick zip imlib2 java-21-openjdk-devel lapack-devel lftp libexif-devel SDL2_gfx-devel meson gd-devel libunistring-devel lzma-sdk-devel libmpc-devel mpfr-devel mpv openblas-devel pv qemu rlwrap sox virt-manager unrar vte-devel zathura zathura-djvu zathura-pdf-mupdf w3m podman lm_sensors kernel-devel kernel-headers dkms acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+wget -qO- https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local
+
+chmod +x ../compilation_list/compilation_list.sh
+./../compilation_list/compilation_list.sh
