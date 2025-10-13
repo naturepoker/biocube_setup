ee962dcb652fedfccb69936d852ae80fd956d996
sung <48851179+naturepoker@users.noreply.github.com>
2025-10-13 14:46:38 -0400

    Update fedora_base_setup.sh
    Changed group install parameter to fit with dnf5 spec

--- dnf-os/fedora_base_setup.sh
+++ dnf-os/fedora_base_setup.sh
@@ -12,7 +12,7 @@ echo "##################################################"
 echo "##################################################"
 echo "##################################################"
 echo "                                                  "
-echo "           SCRIPT UPDATED - JUN 12 2025           "
+echo "           SCRIPT UPDATED - OCT 13 2025           "
 echo "                                                  "
 echo "##################################################"
 
@@ -30,7 +30,7 @@ sudo dnf install -y rpmfusion-free-release-tainted
 sudo dnf install -y rpmfusion-nonfree-release-tainted
 sudo dnf install -y dnf-plugins-core
 sudo dnf -y group upgrade core
-sudo dnf config-manager setopt fedora-cisco-openh264=1
+#sudo dnf config-manager setopt fedora-cisco-openh264=1
 sudo dnf install -y gstreamer1-plugin-openh264 mozilla-openh264
 sudo dnf install lame\* --exclude=lame-devel
 sudo dnf install -y fira-code-fonts 'mozilla-fira*' 'google-roboto*'
@@ -38,8 +38,8 @@ sudo dnf install -y fira-code-fonts 'mozilla-fira*' 'google-roboto*'
 #sudo dnf install akmod-nvidia
 #sudo dnf install xorg-x11-drv-nvidia-cuda
 
-sudo dnf group install --with-optional development-tools
-sudo dnf group install --with-optional c-development
+sudo dnf group install development-tools
+sudo dnf group install c-development
 sudo dnf install @multimedia
 
 sudo dnf install -y imlib2-devel p7zip p7zip-plugins cmake colordiff datamash eigen3-devel flac flatpak gdk-pixbuf2-devel giflib-devel parallel gnuplot gsl-devel pipx htop ImageMagick zip imlib2 java-21-openjdk-devel lapack-devel lftp libexif-devel SDL2_gfx-devel meson gd-devel libunistring-devel lzma-sdk-devel libmpc-devel mpfr-devel mpv openblas-devel pv qemu rlwrap sox virt-manager unrar vte-devel zathura zathura-djvu zathura-pdf-mupdf w3m podman lm_sensors kernel-devel kernel-headers dkms acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig distrobox libcurl-devel boost-devel
