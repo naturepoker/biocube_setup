e05c56635252e8126293ddac678868177c2729cb
naturepoker <48851179+naturepoker@users.noreply.github.com>
2026-01-16 10:38:48 -0500

    major update to handling post-compilation setup

--- apt-os/ubuntu_base_setup.sh
+++ apt-os/ubuntu_base_setup.sh
@@ -5,13 +5,16 @@ setup_date=$(date +"%Y%m%d")
 
 if [[ $(uname -p) == x86_64 ]]; then
 	printf "\n Compatible CPU architecture found"
+	printf "\n"
 else
 	printf "\n Incompatible architecture found. This setup script is meant for x86_64 machines"
+	printf "\n"
 	exit
 fi
 
 if ! command -v apt > /dev/null; then
 	printf "\n This script is meant for Debian/Ubuntu systems with apt command. Exiting"
+	printf "\n"
 	exit
 fi
 
@@ -20,17 +23,17 @@ echo "##################################################"
 echo "##################################################"
 echo "##################################################"
 echo "                                                  "
-echo "           SCRIPT UPDATED - JUL 02 2023           "
+echo "           SCRIPT UPDATED - JAN 15 2026           "
 echo "                                                  "
 echo "##################################################"
 
 sudo apt update
 sudo apt upgrade
-sudo apt install autoconf autoconf-doc automake bison build-essential cmake colordiff curl default-jdk dictd dict-freedict-fra-eng dict-gcide ecasound enscript ffmpeg flac fontconfig gcc gdb gfortran git gnuplot grads graphviz groff gtk-doc-tools htop imagemagick jq lame lftp libisal-dev libboost-all-dev libbz2-dev libcurl4-gnutls-dev libeigen3-dev libexif-dev libffi-dev libfribidi-dev libfuse2 libgc-dev libgdk-pixbuf2.0-dev libgif-dev libglib2.0-dev libgmp-dev libgsl-dev libgtk-3-dev libimage-exiftool-perl libimlib2-dev liblapack-dev liblzma-dev libmagickwand-dev libmpc-dev libmpfr-dev libmpg123-dev libncurses5-dev libopenblas-dev libpcre3-dev libreadline-dev libsdl2-dev libssl-dev libtool libunistring-dev libvirt-daemon libxft-dev lm-sensors lynx m4 m4-doc make mencoder mpv neofetch ninja-build openssh-client openssh-server p7zip-full p7zip-rar parallel perl pipx pv python3-pip python3-testresources python-is-python3 qemu-kvm rlwrap sox sudo texlive-fonts-recommended texlive-plain-generic texlive-xetex unrar unzip virt-manager vlc vorbis-tools xclip zathura zathura-djvu zathura-ps zlib1g zlib1g-dev
+sudo apt install autoconf autoconf-doc automake bat bison build-essential cmake colordiff curl default-jdk dictd dict-freedict-fra-eng dict-gcide ecasound enscript ffmpeg flac fontconfig gcc gdb gfortran git gnuplot grads graphviz groff gtk-doc-tools htop imagemagick jq lame lftp libisal-dev libboost-all-dev libbz2-dev libcurl4-gnutls-dev libeigen3-dev libexif-dev libffi-dev libfribidi-dev libfuse2 libgc-dev libgdk-pixbuf2.0-dev libgif-dev libglib2.0-dev libgmp-dev libgsl-dev libgtk-3-dev libimage-exiftool-perl libimlib2-dev liblapack-dev liblzma-dev libmagickwand-dev libmpc-dev libmpfr-dev libmpg123-dev libncurses5-dev libopenblas-dev libpcre3-dev libreadline-dev libsdl2-dev libssl-dev libtool libunistring-dev libvirt-daemon libxft-dev lm-sensors lynx m4 m4-doc make mencoder mpv neofetch ninja-build nvtop openssh-client openssh-server p7zip-full p7zip-rar parallel perl pipx pv python3-pip python3-testresources python-is-python3 qemu-kvm rlwrap sox sudo texlive-fonts-recommended texlive-plain-generic texlive-xetex unrar unzip virt-manager vlc vorbis-tools xclip xvfb zathura zathura-djvu zathura-ps zlib1g zlib1g-dev
 
 mkdir ~/.local/bin
 
-chmod +x ../compilation_list/compilation_list.sh
-./../compilation_list/compilation_list.sh
+chmod +x ../compilation_list/personal_env_compiliation_list.sh
+./../compilation_list/personal_env_compiliation_list.sh
 
 
---
+++ compilation_list/personal_env_compiliation_list.sh
@@ -0,0 +1,392 @@
+#!/usr/bin/env bash
+
+setup_date=$(date +"%Y%m%d")
+
+echo "##################################################"
+echo "##################################################"
+echo "##################################################"
+echo "                                                  "
+echo "           SCRIPT UPDATED - JAN 16 2026           "
+echo "                                                  "
+echo "##################################################"
+
+cd
+
+mkdir ~/tools
+cd ~/tools
+touch "$setup_date"_setup.log
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up htslib###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/samtools/htslib.git
+cd htslib
+git submodule update --init --recursive
+autoreconf -i
+./configure 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+sudo make install
+cd ~/tools
+rm -rf htslib
+
+sudo ldconfig
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up bedtools2###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/arq5x/bedtools2.git
+cd bedtools2
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+ln -s $PWD/bin/* ~/.local/bin
+cd ~/tools
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up samtools###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/samtools/samtools.git
+cd samtools
+autoheader
+autoconf -Wno-syntax
+./configure 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+ln -s $PWD/samtools ~/.local/bin
+cd ~/tools
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up bcftools###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/samtools/bcftools.git
+cd bcftools
+autoheader && autoconf && ./configure --enable-libgsl 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+ln -s $PWD/bcftools ~/.local/bin
+cd ~/tools
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up hmmer###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+wget http://eddylab.org/software/hmmer/hmmer.tar.gz
+tar zxvf hmmer.tar.gz
+rm hmmer.tar.gz
+cd hmmer-*
+./configure 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+make check 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+sudo make
+ln -s $PWD/src/alimask ~/.local/bin
+ln -s $PWD/src/hmmalign ~/.local/bin
+ln -s $PWD/src/hmmbuild ~/.local/bin
+ln -s $PWD/src/hmmc2 ~/.local/bin
+ln -s $PWD/src/hmmconvert ~/.local/bin
+ln -s $PWD/src/hmmemit ~/.local/bin
+ln -s $PWD/src/hmmerfm-exactmatch ~/.local/bin
+ln -s $PWD/src/hmmfetch ~/.local/bin
+ln -s $PWD/src/hmmlogo ~/.local/bin
+ln -s $PWD/src/hmmpgmd ~/.local/bin
+ln -s $PWD/src/hmmpgmd_shard ~/.local/bin
+ln -s $PWD/src/hmmpress ~/.local/bin
+ln -s $PWD/src/hmmscan ~/.local/bin
+ln -s $PWD/src/hmmsearch ~/.local/bin
+ln -s $PWD/src/hmmsim ~/.local/bin
+ln -s $PWD/src/hmmstat ~/.local/bin
+ln -s $PWD/src/jackhmmer ~/.local/bin
+ln -s $PWD/src/makehmmerdb ~/.local/bin
+ln -s $PWD/src/nhmmer ~/.local/bin
+ln -s $PWD/src/nhmmscan ~/.local/bin
+ln -s $PWD/src/phmmer ~/.local/bin
+
+cd easel
+sudo make
+ln -s $PWD/miniapps/easel ~/.local/bin
+ln -s $PWD/miniapps/esl-afetch ~/.local/bin
+ln -s $PWD/miniapps/esl-alimanip ~/.local/bin
+ln -s $PWD/miniapps/esl-alimap ~/.local/bin
+ln -s $PWD/miniapps/esl-alimask ~/.local/bin
+ln -s $PWD/miniapps/esl-alimerge ~/.local/bin
+ln -s $PWD/miniapps/esl-alipid ~/.local/bin
+ln -s $PWD/miniapps/esl-alirev ~/.local/bin
+ln -s $PWD/miniapps/esl-alistat ~/.local/bin
+ln -s $PWD/miniapps/esl-compalign ~/.local/bin
+ln -s $PWD/miniapps/esl-compstruct ~/.local/bin
+ln -s $PWD/miniapps/esl-construct ~/.local/bin
+ln -s $PWD/miniapps/esl-histplot ~/.local/bin
+ln -s $PWD/miniapps/esl-mask ~/.local/bin
+ln -s $PWD/miniapps/esl-mixdchlet ~/.local/bin
+ln -s $PWD/miniapps/esl-reformat ~/.local/bin
+ln -s $PWD/miniapps/esl-selectn ~/.local/bin
+ln -s $PWD/miniapps/esl-seqrange ~/.local/bin
+ln -s $PWD/miniapps/esl-seqstat ~/.local/bin
+ln -s $PWD/miniapps/esl-sfetch ~/.local/bin
+ln -s $PWD/miniapps/esl-shuffle ~/.local/bin
+ln -s $PWD/miniapps/esl-ssdraw ~/.local/bin
+ln -s $PWD/miniapps/esl-translate ~/.local/bin
+ln -s $PWD/miniapps/esl-weight ~/.local/bin
+
+cd ~/tools
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up pfam-A###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+mkdir pfam
+cd pfam
+lftp -c get ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz
+gunzip Pfam-A.hmm.gz
+hmmpress Pfam-A.hmm 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+cd ~/tools
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up seqtk###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/lh3/seqtk.git
+cd seqtk
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+mv seqtk ~/.local/bin
+cd ~/tools
+rm -rf seqtk
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up filtlong###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/rrwick/Filtlong.git
+cd Filtlong
+#echo "possible gcc13 build failure - add -#include <cstdint>- to kmer.h"
+#https://github.com/rrwick/Filtlong/pull/39
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+mv bin/filtlong ~/.local/bin
+cd ~/tools
+rm -rf Filtlong
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up minimap###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/lh3/minimap2.git
+cd minimap2
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+mv minimap2 ~/.local/bin
+cd ~/tools
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up miniprot###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/lh3/miniprot.git
+cd miniprot
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+mv miniprot ~/.local/bin
+cd ~/tools
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up bwa###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/lh3/bwa.git
+cd bwa
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+ln -s $pwd/bwa ~/.local/bin
+cd ~/tools
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up lastalign###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://gitlab.com/mcfrith/last.git
+cd last
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+ln -s $PWD/bin/* ~/.local/bin
+cd ~/tools
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up bioawk###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/lh3/bioawk.git
+cd bioawk
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+cp bioawk ~/.local/bin
+cd ~/tools
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up trimal###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/inab/trimal.git
+cd trimal/source
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+ln -s $PWD/trimal ~/.local/bin
+ln -s $PWD/statal ~/.local/bin
+ln -s $PWD/readal ~/.local/bin
+cd ~/tools
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up iqtree3###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/iqtree/iqtree3.git
+cd iqtree3
+git submodule init
+git submodule update
+mkdir build
+cd build
+cmake ..
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+ln -s $PWD/iqtree3 ~/.local/bin
+cd ~/tools
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up muscle5###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/rcedgar/muscle.git
+cd muscle/src
+#make -j 1 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+chmod +x build_linux.bash
+./build_linux.bash
+cd ..
+ln -s $PWD/bin/muscle ~/.local/bin
+cd ~/tools
+
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###Setting up FastANI###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+echo "###" 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+git clone https://github.com/ParBLiSS/FastANI.git
+cd FastANI
+./bootstrap.sh 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+./configure 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+make 2>&1 | tee -a ~/tools/"$setup_date"_setup.log
+cp fastANI ~/.local/bin
+cd ~/tools
+rm -rf FastANI
+
+wget http://www.microbesonline.org/fasttree/FastTreeMP
+chmod +x FastTreeMP
+mv FastTreeMP ~/.local/bin
+
+wget http://opengene.org/fastp/fastp
+chmod +x fastp
+mv fastp ~/.local/bin
+
+wget https://github.com/shenwei356/taxonkit/releases/download/v0.20.0/taxonkit_linux_amd64.tar.gz
+tar zxvf taxonkit_linux_amd64.tar.gz
+rm taxonkit_linux_amd64.tar.gz
+mv taxonkit ~/.local/bin
+
+lftp -c get ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
+tar zxvf taxdump.tar.gz
+mkdir -p $HOME/.taxonkit
+mv names.dmp nodes.dmp delnodes.dmp merged.dmp $HOME/.taxonkit
+rm citations.dmp division.dmp gc.prt gencode.dmp images.dmp readme.txt taxdump.tar.gz
+
+wget https://github.com/shenwei356/seqkit/releases/download/v2.11.0/seqkit_linux_amd64.tar.gz
+tar zxvf seqkit_linux_amd64.tar.gz
+rm seqkit_linux_amd64.tar.gz
+mv seqkit ~/.local/bin
+
+wget https://mmseqs.com/latest/mmseqs-linux-sse41.tar.gz
+tar zxvf mmseqs-linux-sse41.tar.gz
+ln -s ~/tools/mmseqs/bin/mmseqs ~/.local/bin/mmseqs
+rm mmseqs-linux-sse41.tar.gz
+
+git clone https://github.com/mpdunne/alan.git
+mv alan/alan ~/.local/bin
+rm -rf alan
+
+wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.17.0+-x64-linux.tar.gz
+tar zxvf ncbi-blast-2.17.0+-x64-linux.tar.gz
+cp ncbi-blast-2.17.0+/bin/* ~/.local/bin
+rm -rf ncbi-blast-2.17.0+-x64-linux.tar.gz ncbi-blast-2.17.0+
+
+#entrez is being phased out - this portion will be removed soon
+#wget through ftp does not seem to work anymore on fedora 40 onward
+#sh -c "$(wget -q ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/install-edirect.sh -O -)"
+lftp -c get ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/install-edirect.sh
+chmod +x install-edirect.sh
+./install-edirect.sh
+rm install-edirect.sh
+
+#datasets from NCBI as entrez replacement
+lftp -c get https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linux-amd64/datasets
+chmod +x datasets
+mv datasets ~/.local/bin
+
+git clone https://github.com/muennich/sxiv.git
+cd sxiv
+make -j
+mv sxiv ~/.local/bin
+cd ~/tools
+rm -rf sxiv
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+source ~/.bashrc
+
+git clone https://github.com/mbhall88/rasusa.git
+cd rasusa
+cargo build --release
+cargo test --all
+cp target/release/rasusa ~/.local/bin/
+cd ~/tools
+rm -rf rasusa
+
+git clone https://github.com/fulcrumgenomics/fqgrep.git
+cd fqgrep
+cargo build --release
+mv target/release/fqgrep ~/.local/bin
+cd ~/tools
+rm -rf fqgrep
+
+pipx install pyfaidx
+pipx install html2text
+pipx install termvisage
+pipx install bpytop
+
+#alternate shell choices - wezterm vs kitty
+#curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+#ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/kitty
+
+git clone https://github.com/hackerb9/lsix.git
+mv lsix/lsix ~/.local/bin
+rm -rf lsix
+
+git clone https://github.com/hackerb9/vv.git
+cp vv/vv ~/.local/bin
+rm -rf vv
+
+git clone https://github.com/atanunq/viu.git
+cd viu
+cargo install --path .
+cp target/release/viu ~/.local/bin
+cd ~/tools
+rm -rf viu
+
+git clone --recursive --branch release https://git.skyjake.fi/gemini/lagrange
+mkdir lagrange_browser
+cd lagrange_browser
+cmake ~/tools/lagrange -DCMAKE_BUILD_TYPE=Release
+cmake --build .
+ln -s ~/tools/lagrange_browser/lagrange ~/.local/bin/lagrange
+cd ~/tools
+rm -rf lagrange
+
+wget https://github.com/makew0rld/amfora/releases/download/v1.11.0/amfora_1.11.0_linux_64-bit
+mv amfora_1.11.0_linux_64-bit amfora
+chmod +x amfora
+mv amfora ~/.local/bin
+
+curl -fsSL https://install.julialang.org | sh
+
+cat << 'EOF'
+#julia environment setup
+#.julia/config/startup.jl
+#before adding startup script, add OhMyREPL, Revise to base env
+
+using Pkg:Pkg
+atreplinit() do repl
+try
+	@eval using OhMyREPL
+	catch e
+	@warn "error while importing OhMyREPL" exception=(e, catch_backtrace())
+end
+end
+
+atreplinit() do repl
+try
+	@eval using Revise
+	catch e
+	@warn "error while importing Revise" exception=(e, catch_backtrace())
+end
+end
+EOF
+
